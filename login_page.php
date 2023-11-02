8b2d261533b910c17f61a968dfe4545dd029dca4
markplnivo <markjpenaojas@gmail.com>
2023-11-02 17:27:08 +0800

    Update login_page.php

--- login_page.php
+++ login_page.php
@@ -1,19 +1,26 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Login Page</title>
+<title>Login Page</title>
     <style>
         /* Add your CSS styling here */
         body {
-            font-family: Arial, sans-serif;
+    font-family: Arial, sans-serif;
+    background-color: rgba(70,60,52,1.00);
         }
         .container {
-            max-width: 400px;
-            margin: 0 auto;
-            padding: 20px;
-            border: 1px solid #ccc;
-            border-radius: 5px;
+    margin: 0 auto;
+    padding: 20px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    background-color: rgba(94,94,94,1.00);
+    min-width: 10%;
+    max-width: 30%;
         }
+
+.container h2 {
+    text-align: center;
+}
         input[type="text"], input[type="password"] {
             width: 100%;
             padding: 10px;
@@ -21,42 +28,63 @@
             border: 1px solid #ccc;
             border-radius: 3px;
         }
+form p login_username_password {
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    font-size: large;
+}
+.container form #username {
+    width: 60%;
+}
+.container form #password {
+    width: 60%;
+}
         .center-button {
-            text-align: center;
+    text-align: center;
+    margin-left: auto;
+    margin-right: auto;
         }
         button {
-            padding: 10px;
-            background-color: #007BFF;
-            color: #fff;
-            border: none;
-            border-radius: 3px;
-            cursor: pointer;
+    background-color: #007BFF;
+    color: #fff;
+    border: none;
+    border-radius: 5px;
+    padding-top: 3%;
+    padding-right: 5%;
+    padding-left: 5%;
+    padding-bottom: 3%;
+    cursor: pointer;
         }
     </style>
 </head>
 <body>
     <div class="container">
-        <h2>Login</h2>
+        <h2>eWork Login</h2>
         <form action="login.php" method="post">
-            <label for="username">Username:</label>
-            <input type="text" name="username" id="username" required>
-            
-            <label for="password">Password:</label>
-            <input type="password" name "password" id="password" required>
-
+            <p>
+              <login_username_password>
+                <label for="username">Username:</label>
+                <input type="text" name="username" id="username" required>
+                <label for="password">Password:</label>
+                <input type="password" name "password" id="password" required>
+              </login_username_password>
+            </p>
             <div class="center-button">
-                <button type="submit">Login</button>
+              <button type="submit">Login</button>
             </div>
-        </form>
+      </form>
 
         <div>
-            <a href="forgot_password.php">Forgot Password?</a>
+          <p><a href="forgot_password.php">Forgot Password?</a></p>
         </div>
 
         <div>
             <p>Don't have an account? <a href="register.php">Register</a></p>
         </div>
     </div>
+   
 </body>
 </html>
 
